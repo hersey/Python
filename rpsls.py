c40163af91c25604b170eb0d30f2a84ed5785e4b
hersey <liu.hersey@gmail.com>
2014-11-01 15:03:26 -0500

    Revision on Rock-Paper-Scissors-Lizard-Spock game
    The following changes are made for my second version of RPSLS:
    -Fixed the problem if user input is out of the five words: give an
    invalid warning.
    -Added a loop so the game can continue.
    -Added counter to keep track of number of tries, users win/tie against
    the computer.
    -Give user exit point when each new round starts.
    -Count down the number of rounds played (5 times) and give summary at
    the end of the game or when exit voluntarily.

--- rpsls.py
+++ rpsls.py
@@ -16,24 +16,25 @@ Rock crushes Scissors
 See coursera instruction for more detail: 
 https://class.coursera.org/interactivepython-005/human_grading/view/courses/972530/assessments/28/submissions
 
+
+The key idea of this program is to equate the strings
+"rock", "paper", "scissors", "lizard", "Spock" to numbers
+as follows:
+0 - rock
+1 - Spock
+2 - paper
+3 - lizard
+4 - scissors
+
+
+The following code is my second version of RPSLS:
+-Fixed the problem if user input is out of the five words
+-Added a loop so the game can continue
+-Added counter to keep track of number of tries, users win/tie against the computer. 
 '''
 
-# The key idea of this program is to equate the strings
-# "rock", "paper", "scissors", "lizard", "Spock" to numbers
-# as follows:
-#
-# 0 - rock
-# 1 - Spock
-# 2 - paper
-# 3 - lizard
-# 4 - scissors
+#Ask User for input and store in variable "Name" and translate word to number.
 
-'''
-The following code is my first version of RPSLS:
--Haven't fixed the problem if user input is out of the five words
--Need to add a loop so the game can continue
--Need to make the code shorter and simpler
-'''
 
 def name_to_num(name):
 	if name=="rock":
@@ -46,11 +47,15 @@ def name_to_num(name):
 		return 3
 	elif name=="scissors":
 		return 4
+	elif name=="exit":
+		return 200 
 	else:
-		return "You entered an invalid word. Try again."   # need to figure out how to stop the following functions if input something wrong) 
+		return 100
 
 
-def num_to_name(num):
+#Computer randomly generate a number between 0 and 4.
+import random
+def num_to_name(num):    #Translate the number into word
 	if num==0:
 		return "rock"
 	elif num == 1:
@@ -63,24 +68,49 @@ def num_to_name(num):
 		return "scissors"
 
 
-#Ask User for input and store in variable "Name" and translate word to 
-name=str(raw_input("Choose one out of 'rock, paper, scissors, lizard or spock': "))
-player=int(name_to_num(name))
-#Generate random number from 0 to 4 and assign number to a word using num_to_name function
-
-import random
-machine=random.randint(0,4)
-print "Your computer chose " + str(num_to_name(machine)) + "."  #Show user what machine chose.
-
 #Use function rpsls() to compare the input of user vs machine
+
 def rpsls(p, m):
-	if(p-m)%5 in (1,2):
-		return "Congrats, you win! Go human!"
+	if p==100: 
+		return "You entered an invalid word. Try again."   # Check on invalid input from users.
+	elif p==200:
+		print "Thanks for playing."    #Give user an exit to quit the game everytime the game restarts. 
+		exit()
+	elif(p-m)%5 in (1,2):
+		return "Congrats, you win. Go human!"
 	elif (p-m)%5 in (3,4):
 		return "Sorry, computer wins. Go AI!"
 	elif p==m:
 		return "It's a tie!"
-	else: 
-		return "I don't know what you just did."
+	else:
+		return none
 
-print rpsls(player,machine)
+#user_win variable keeps a count on how many times user win. Tries keeps a count of how many times user has played. 
+ 
+def main():
+	print "Welcome to Sheldon Cooper's famous Rock-Paper-Scissors-Lizard-Spock game."
+	print "Here're the rules:\nScissors cut Paper\nPaper covers Rock\nRock crushes Lizard \nLizard poisons Spock \n\
+Spock smashes Scissors \nScissors decapitate Lizard \nLizard eats Paper \nPaper disproves Spock \nSpock vaporizes Rock \nRock crushes Scissors." 
+	print "You have 3 times to play with the computer. Type 'exit' to quit the game."
+	user_win=0
+	tie=0
+	tries=1
+	while tries<=3: 
+		player=name_to_num(str(raw_input("Choose one out of 'rock, paper, scissors, lizard or spock': ")))
+		if player==200:
+			print "Thanks for playing. We'll miss you. You won "+str(user_win)+" times and tied "+ str(tie)+" times."
+			exit()
+		machine=random.randint(0,4)
+		print "Your computer chose " + str(num_to_name(machine)) + "."   #Show user what machine chose.
+		print rpsls(player,machine)
+		if rpsls(player,machine)=="Congrats, you win. Go human!":
+			user_win+=1
+		elif rpsls(player,machine)=="It's a tie!":
+			tie+=1
+		print "You have "+ str(3-tries) + " more chances."
+		tries+=1
+	print "Game Over. You won the computer "+str(user_win)+" times and tied "+ str(tie)+" times."
+
+main()
+
+
