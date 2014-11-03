ba7b1612c3ee5cab72be74bd54af1761d0f041c3
hersey <liu.hersey@gmail.com>
2014-11-03 09:48:06 -0600

    New problem-print back the sentence

---
+++ printback.py
@@ -0,0 +1,22 @@
+'''
+Practice Problem: 15
+
+Write a program (using functions!) that asks the user for a long string containing multiple words. Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+
+My name is Michele
+Then I would see the string:
+Michele is name My
+shown back to me.
+
+Link: http://www.practicepython.org/exercise/2014/05/21/15-reverse-word-order.html
+'''
+
+user=raw_input("Give me a sentence: ")
+
+def backward(x):
+	s=x.split()
+	#return ' '.join(s[::-1])  #can also use reversed()
+	return ' '.join(reversed(s))
+
+print backward(user)
+
